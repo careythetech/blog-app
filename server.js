6b6ee0394473a8a421246837021b59bdfd382a5d
Careytech <careythetech@gmail.com>
2020-10-19 20:24:02 -0400

    made changes

--- server.js
+++ server.js
@@ -32,15 +32,15 @@ app.use('/api/covid/', router);
 app.use('/api/politic/', politicRouter);
 app.use('/api/sport/', sportRouter);
 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+// if (process.env.NODE_ENV === 'production') {
+//     // Exprees will serve up production assets
+//     app.use(express.static('client/build'));
+//     // Express serve up index.html file if it doesn't recognize route
+//     const path = require('path');
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+// }
 
 
 const port = process.env.PORT || 3002;
